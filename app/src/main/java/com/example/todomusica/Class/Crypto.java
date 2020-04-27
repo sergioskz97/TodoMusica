062eec27a2a0b52593e2044a062afbeedb56f119
SkyZyy <sergito1997.sd@gmail.com>
2020-04-27 10:20:39 +0100

    Changig cryp method

--- app/src/main/java/com/example/todomusica/Class/Crypto.java
+++ app/src/main/java/com/example/todomusica/Class/Crypto.java
@@ -1,22 +1,83 @@
-package com.example.todomusica.Class;
+package com.example.alexistorres.Class;
 
-public class Crypto{
+import android.os.Build;
 
-    public byte[] encrypt(byte[] data){
-        byte[] enc = new  byte[data.length];
+import androidx.annotation.RequiresApi;
 
-        for (int i=0; i<data.length; i++){
-            enc[i] = (byte) ((i % 2 == 0) ? data[i] + 1 : data[i] -1);
-        }
-        return enc;
+import org.apache.commons.codec.binary.Base64;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Crypto {
+
+    public Crypto () {}
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String encrypt (String pass) throws  Exception {
+        String key = "alexispelu";
+        byte[] iBytes;
+
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        byte[] saltBytes = bytes;
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec spec = new PBEKeySpec(key.toCharArray(),saltBytes,65556,256);
+        SecretKey secretKey = factory.generateSecret(spec);
+        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        AlgorithmParameters params = cipher.getParameters();
+        iBytes =   params.getParameterSpec(IvParameterSpec.class).getIV();
+        byte[] encryptedTextBytes = cipher.doFinal(pass.getBytes(StandardCharsets.UTF_8));
+
+        byte[] buffer = new byte[saltBytes.length + iBytes.length + encryptedTextBytes.length];
+        System.arraycopy(saltBytes, 0, buffer, 0, saltBytes.length);
+        System.arraycopy(iBytes, 0, buffer, saltBytes.length, iBytes.length);
+        System.arraycopy(encryptedTextBytes, 0, buffer, saltBytes.length + iBytes.length, encryptedTextBytes.length);
+
+        return new Base64().encodeToString(buffer);
     }
 
-    public byte[] decrypt(byte[] data){
-        byte[] enc = new byte[data.length];
+    public String decrypt (String pass) throws Exception {
+        String key="alexispelu";
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        ByteBuffer buffer = ByteBuffer.wrap(new Base64().decode(pass));
+        byte[] saltBytes = new byte[20];
+        buffer.get(saltBytes, 0, saltBytes.length);
+        byte[] ivBytes1 = new byte[cipher.getBlockSize()];
+        buffer.get(ivBytes1, 0, ivBytes1.length);
+        byte[] encryptedTextBytes = new byte[buffer.capacity() - saltBytes.length - ivBytes1.length];
 
-        for (int i=0; i<data.length; i++){
-            enc[i] = (byte) ((i % 2 == 0) ? data[i] - 1 : data[i]+1);
+        buffer.get(encryptedTextBytes);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec spec = new PBEKeySpec(key.toCharArray(), saltBytes, 65556, 256);
+        SecretKey secretKey = factory.generateSecret(spec);
+        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(ivBytes1));
+        byte[] decryptedTextBytes = null;
+        try {
+            decryptedTextBytes = cipher.doFinal(encryptedTextBytes);
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
         }
-        return enc;
+
+        return new String(decryptedTextBytes);
     }
 }
