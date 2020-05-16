e5008e75a3afb5478bc06a507dfc2bc63c59a510
SkyZyy <sergito1997.sd@gmail.com>
2020-05-16 18:24:39 +0100

    Notification v2

--- app/src/main/java/com/example/todomusica/Class/NotificationUpdater.java
+++ app/src/main/java/com/example/todomusica/Class/NotificationUpdater.java
@@ -1,10 +1,14 @@
 package com.example.todomusica.Class;
 
+import android.app.NotificationManager;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.example.todomusica.R;
 
 public class NotificationUpdater extends Worker {
 
@@ -16,7 +20,24 @@ public class NotificationUpdater extends Worker {
 
     @Override
     public Result doWork() {
-        
+        String artist = "artist";
+
+        NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
+                new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.login_logo)
+                .setContentTitle("Nuevas noticias disponibles")
+                .setContentText("Nuevas noticias sobre" + artist);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService();
+
+        notificationManager.notify(0, mbuilder.build());
+
         return Result.success();
+    }
+
+    private Object getSystemService() {
+        Object object = null;
+
+        return object;
     }
 }
